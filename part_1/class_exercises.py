264684cfd4bab78faf712b3f7d0ff857573625ad
macrtk <mac.rutkowski@gmail.com>
2023-12-28 16:03:38 +0100

    new class_exercises

--- part_1/class_exercises.py
+++ part_1/class_exercises.py
@@ -1,3 +1,5 @@
+## SIMPLE CLASS DESCRIBING A PERSON WITH 2 ATTRIBUTES
+
 class Person():
     def __init__(self,name,age):
         self.name = name
@@ -10,3 +12,36 @@ class Person():
 person1 = Person("Matt",35)
 person1
 Person.greet(person1)
+
+### SAMPLE CLASS FOR DA
+
+class DataAnalyzer:
+    def __init__(self):
+        # Initializer for the class
+        self.data = []
+
+    def load_data(self, data):
+        # Method to load data into the class
+        self.data = data
+
+    def calculate_average(self):
+        # Method to calculate the average of the data
+        if not self.data:
+            return "No data loaded"
+        return sum(self.data) / len(self.data)
+
+    def data_summary(self):
+        # Method to display a summary of the data
+        if not self.data:
+            return "No data to summarize"
+        return f"Data: {self.data}\nAverage: {self.calculate_average()}"
+
+# Create an instance of the DataAnalyzer class
+analyzer = DataAnalyzer()
+
+# Load some sample data
+sample_data = [10, 20, 30, 40, 50]
+analyzer.load_data(sample_data)
+
+# Display the data summary
+analyzer.data_summary()
